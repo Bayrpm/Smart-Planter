8aae11ae8739b1f872f2628ef033f7f22a01a491
WolfXthaX <135666526+WolfXthaX@users.noreply.github.com>
2023-11-28 22:32:14 -0300

    Update views.py

--- Backend/SmartPlanter/api/views.py
+++ Backend/SmartPlanter/api/views.py
@@ -1,18 +1,25 @@
-# views.py
 from rest_framework import generics
 from .models import SensorData
 from .serializers import SensorDataSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
-
 class ListaSensorData(generics.ListCreateAPIView):
-    queryset = SensorData.objects.all()
     serializer_class = SensorDataSerializer
-    
+
+    def get_queryset(self):
+        # Devolver el primer registro si existe
+        return SensorData.objects.all().first()
 
     def perform_create(self, serializer):
-        if serializer.is_valid():
-            serializer.save()
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # Obtener el primer registro
+        existing_data = SensorData.objects.first()
+
+        # Si existe un registro, actualizar los valores
+        if existing_data:
+            serializer.update(existing_data, self.request.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        # Si no existe un registro, crear uno nuevo
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
